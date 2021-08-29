e27da7808fa9bdc8dc057c7f9c55a68503e853bb
amrezz13 <romaniga1325@gmail.com>
2021-08-29 14:32:05 +0200

    new

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-/*
- * Programming Quiz: Using the Rest Parameter (1-5)
- */
-
-// your code goes here
-function average(...nums) {
-    let sum =0;
-
-    for( let num of nums){
-        sum += num;
-    }    
-    return sum/nums.length;
-}
-
-console.log(average(2, 6));
-console.log(average(2, 3, 3, 5, 7, 10));
-console.log(average(7, 1432, 12, 13, 100));
-console.log(average());
+$.getJSON("test.json", function(json) {
+  exam = json; // this will show the info it in firebug console
+});
